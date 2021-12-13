d370ffbc474a20d8034583593a510b34c2834abc
Alexandr Voishel <avoishel@gmail.com>
2021-12-13 23:39:39 +0300

    Add Packages

--- src/Basic/JavaTest.java
+++ src/Basic/JavaTest.java
@@ -1,3 +1,5 @@
+package Basic;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -6,7 +8,7 @@ public class JavaTest {
     public static void main(String []args){
 
         List<Double> ints = new ArrayList<Double>();
-        List<? super Number> nums = ints;
+        //List<? super Number> nums = ints;
 
         System.out.println("ok");
     }
